e3a1d65bf1479dbeef7da7fa77a9a6317ca7916a
SaiAnshuman <71771487+SaiAnshuman@users.noreply.github.com>
2020-11-17 15:59:20 +0530

    Add files via upload

--- lol.js
+++ lol.js
@@ -11,3 +11,46 @@ var firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  user_name= localStorage.getItem("user_name");
+
+    document.getElementById("username").innerHTML = "Welcome " + user_name + "!";
+
+    function addRoom(){
+
+       room_name = document.getElementById("room_name").value;
+       firebase.database().ref("/").child(room_name).update({
+         purpose:"adding room name"
+       });
+       localStorage.setItem("roomname",room_name);
+       window.location = "kwitter_page.html";
+
+    }
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      console.log("Room-name: " + Room_names);
+      row = "<div class='room_name' id="+Room_names+"onclick='reDirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+
+      //End code
+      });});}
+getData();
+
+function reDirectToRoomName(name){
+
+  console.log(name);
+  localStorage.setItem("room_name",name);
+  window.location = "LOL!_page.html";
+
+}
+
+function logOut(){
+
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("roomname");
+  window.location = "index.html";
+
+}
