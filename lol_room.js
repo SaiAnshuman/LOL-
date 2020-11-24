59bdbb3d36995b9bb3e3ea992cc44563ca35db89
SaiAnshuman <71771487+SaiAnshuman@users.noreply.github.com>
2020-11-24 13:55:53 +0530

    Add files via upload

--- lol_room.js
+++ lol_room.js
@@ -25,7 +25,7 @@ function addRoom()
 
     localStorage.setItem("room_name", room_name);
     
-    window.location = "kwitter_page.html";
+    window.location = "lol_page.html";
 }
 
 function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
